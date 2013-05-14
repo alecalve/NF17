9bac650c3e03aede8d3c64d47414bb048dc13abd
alecalve <antoine@alc.io>
2013-05-14 23:28:17 +0200

    Ajout d'un début d'interface admin

---
+++ view/admin.php
@@ -0,0 +1,15 @@
+<?php
+include(dirname(__FILE__).'/head.php');
+?>
+        <div class="row">
+			<div class="span12">
+				<div class="hero-unit">
+					<h2>Bienvenue sur le site de Météo France</h2>
+					<ul class="nav nav-pills">
+						<li><a href="index.php">Vue utilisateur</a></li>
+						<li class="active"><a href="admin.php">Vue administration</a></li>
+					</ul>
+				</div>
+			</div>
+        </div>
+<?php include(dirname(__FILE__).'/tail.php'); ?>
