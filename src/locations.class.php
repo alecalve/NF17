e45f5c08c5ea7272d0496adb684608fed2f41aee
alecalve <antoine@alc.io>
2013-05-16 00:24:11 +0200

    Ajout de la gestions des départements et régions

---
+++ src/locations.class.php
@@ -0,0 +1,36 @@
+<?php
+include_once(dirname(__FILE__).'/utils/Bdd.class.php');
+include_once(dirname(__DIR__).'/conf.php');
+
+class LocationsManager
+{
+	
+	private $db;
+    
+    public function __construct() {
+		global $_CONF;
+		$this->db = new Bdd($_CONF['db_host'], $_CONF['db_name'], $_CONF['db_user'], $_CONF['db_pass']);
+	}
+		 
+    public function getDepartements() {
+		$request = $this->db->prepare("SELECT * FROM tDepartements");
+		$success = $this->db->execute($request, array());
+		if (!$success) {
+			throw new Exception("Impossible de trouver la liste des départements");
+        }
+        $array = $request->fetchAll(PDO::FETCH_ASSOC);
+        return $array;        
+    }
+    
+    public function getRegions() {
+		$request = $this->db->prepare("SELECT * FROM tRegion");
+		$success = $this->db->execute($request, array());
+		if (!$success) {
+			throw new Exception("Impossible de trouver la liste des régions");
+        }
+        $array = $request->fetchAll(PDO::FETCH_ASSOC);
+        return $array;     
+    }
+}
+
+
