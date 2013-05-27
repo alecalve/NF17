037ff565187b09552e50446bb14be40855bc3b82
alecalve <antoine@alc.io>
2013-05-27 22:40:27 +0200

    Changement du titre de head_admin

--- view/head_admin.php
+++ view/head_admin.php
@@ -1,7 +1,7 @@
         <div class="row">
             <div class="span12">
                 <div class="hero-unit">
-                    <h2>Bienvenue sur le site de Météo France</h2>
+                    <h2>Bienvenue sur le site de Météo NF17</h2>
                     <ul class="nav nav-pills">
                         <li><a href="index.php">Vue utilisateur</a></li>
                         <li class="active"><a href="admin.php">Vue administration</a></li>
