6c377907c8fbd0fe5b1cbdd3e65a3c7a3d3ef466
Anurad <arnaud.jelmoni@gmail.com>
2013-06-15 16:49:14 +0300

    Update stats.class.php
    Changement des fonction de requetes

--- src/stats.class.php
+++ src/stats.class.php
@@ -4,15 +4,80 @@ include_once(dirname(__FILE__).'/utils/BaseManager.class.php');
 class StatsManager extends BaseManager
 {
     
-    public function getMeanTemp($lieu, $start, $end) {
+    public function getMeanTempLieu($lieu, $start, $end) {
         //TODO : chercher syntaxe BETWEEN
-        $query = "SELECT AVG(temp) FROM tPrevision WHERE typeprevision = 'température' AND dateprevision >= ? AND dateprevision <= ? AND nom = ?";
+        $query = "SELECT AVG(temp) FROM tPrevision 
+                  WHERE typeprevision = 'température' 
+                  AND dateprevision >= ? 
+                  AND dateprevision <= ? 
+                  AND nom = ?";
         return self::getRequest($query, array($start, $end, $lieu), "Erreur dans l’obtention de la moyenne des températures");
     }
     
-    public function getMeanWind($lieu, $start, $end) {
-        $query = "SELECT AVG(force) FROM tPrevision WHERE typeprevision = 'vent' AND dateprevision >= ? AND dateprevision <= ? AND nom = ?";
+    public function getMeanWindLeu($lieu, $start, $end) {
+        $query = "SELECT AVG(force) 
+                    FROM tPrevision 
+                    WHERE typeprevision = 'vent' 
+                    AND dateprevision >= ? 
+                    AND dateprevision <= ? 
+                    AND nom = ?";
         return self::getRequest($query, array($start, $end, $lieu), "Erreur dans l’obtention de la moyenne des vents");
+    }
+    
+     public function getMeanTempDep($dep, $start, $end) {
+        //TODO : chercher syntaxe BETWEEN
+        $query = "SELECT AVG(temp) 
+                  FROM tPrevision 
+                  INNER JOIN tjMassifDepartement ON tjMassifDepartement.massif = tPrevision.nom
+                  INNER JOIN tVille ON tVille.fkLieu = tPrevion.nom
+                  WHERE typeprevision = 'température' 
+                  AND dateprevision >= ? 
+                  AND dateprevision <= ? 
+                  AND (tjMassifDepartement.departement =? OR tVille.fkDepartement  = ?)";
+        return self::getRequest($query, array($start, $end, $dep,$dep), "Erreur dans l’obtention de la moyenne des températures");
+    }
+    
+    public function getMeanWindDep($dep, $start, $end) {
+        $query = "SELECT AVG(force) 
+                  FROM tPrevision 
+                  INNER JOIN tjMassifDepartement ON tjMassifDepartement.massif = tPrevision.nom
+                  INNER JOIN tVille ON tVille.fkLieu = tPrevion.nom
+                  WHERE typeprevision = 'vent' 
+                  AND dateprevision >= ? 
+                  AND dateprevision <= ? 
+                  AND (tjMassifDepartement.departement =? OR tVille.fkDepartement  = ?)";
+        return self::getRequest($query, array($start, $end, $dep, $dep), "Erreur dans l’obtention de la moyenne des vents");
+    }
+    
+     public function getMeanTempRegion($region, $start, $end) {
+        //TODO : chercher syntaxe BETWEEN
+        $query = "SELECT AVG(temp) 
+                  FROM tPrevision 
+                  INNER JOIN tjMassifDepartement ON tjMassifDepartement.massif = tPrevision.nom
+                  INNER JOIN tVille ON tVille.fkLieu = tPrevion.nom
+                  INNER JOIN tDepartement ON 
+                    (tjMassifDepartement.departement= tDepartement.nom 
+                    OR tVille.fkDepartement = tDepartement.nom)
+                  WHERE typeprevision = 'température' 
+                  AND dateprevision >= ? 
+                  AND dateprevision <= ? 
+                  AND tDepartement.fkRegion =?";
+        return self::getRequest($query, array($start, $end, $region), "Erreur dans l’obtention de la moyenne des températures");
+    }
+    
+    public function getMeanWindRegion($region, $start, $end) {
+        $query = "SELECT AVG(force) 
+                  FROM tPrevision 
+                  INNER JOIN tjMassifDepartement ON tjMassifDepartement.massif = tPrevision.nom
+                  INNER JOIN tVille ON tVille.fkLieu = tPrevion.nom
+                  INNER JOIN tDepartement ON 
+                    (tjMassifDepartement.departement= tDepartement.nom 
+                    OR tVille.fkDepartement = tDepartement.nom)
+                  WHERE typeprevision = 'vent' 
+                  AND dateprevision >= ? 
+                  AND dateprevision <= ? 
+                  AND tDepartement.fkRegion =?";
+        return self::getRequest($query, array($start, $end, $region), "Erreur dans l’obtention de la moyenne des vents");
     }
     
     //Si force est NULL, renvoie le lieu avec la plus grande moyenne de vent
